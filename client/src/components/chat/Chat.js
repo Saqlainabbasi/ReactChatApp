baa2ea59c05e758a68b275826885c45e4489f670
Saqlainabbasi <saqlaina527@gmail.com>
2020-11-16 11:19:23 +0500

    clientChangetoHeroku

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({location}) => {
     const [message, setMessage] = useState('');
 
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:3001';
+    const ENDPOINT = 'https://socket-real-time-chat-app.herokuapp.com/';
 
     useEffect(() => {
         const {name,room} = queryString.parse(location.search);
